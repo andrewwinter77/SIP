782b2747afe4f9f20bfb8fdf33de232cead50f3d
Andrew <andrew@Andrews-MacBook-Air.local>
2013-03-29 17:01:50 +0000

    Initial checkin.

---
+++ SampleAppRouter/src/main/java/org/andrewwinter/sip/approuter/App.java
@@ -0,0 +1,13 @@
+package org.andrewwinter.sip.approuter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

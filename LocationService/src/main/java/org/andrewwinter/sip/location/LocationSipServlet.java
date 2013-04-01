94b887619870e5be40eaa2fa79855dccc57a9312
andrew <andrew@192.168.1.65>
2013-04-01 16:12:46 +0100

    Initial checkin of Location Service - a message proxying service that uses registration bindings to discover where to proxy to.

---
+++ LocationService/src/main/java/org/andrewwinter/sip/location/LocationSipServlet.java
@@ -0,0 +1,58 @@
+package org.andrewwinter.sip.location;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.sip.Proxy;
+import javax.servlet.sip.SipFactory;
+import javax.servlet.sip.SipServlet;
+import static javax.servlet.sip.SipServlet.SIP_FACTORY;
+import javax.servlet.sip.SipServletRequest;
+import javax.servlet.sip.SipURI;
+import javax.servlet.sip.URI;
+
+@javax.servlet.sip.annotation.SipServlet
+public class LocationSipServlet extends SipServlet {
+    
+    private static BindingsManager getBindingsManager() {
+        try {
+            final InitialContext ic = new InitialContext();
+            return (BindingsManager) ic.lookup("java:global/LocationService-1.0-SNAPSHOT/BindingsManager!org.andrewwinter.sip.location.BindingsManager");
+        } catch (NamingException e) {
+            return null;
+        }
+    }
+    
+    private SipFactory getSipFactory() {
+        return (SipFactory) getServletContext().getAttribute(SIP_FACTORY);
+    }
+    
+    @Override
+    protected void doInvite(final SipServletRequest invite) throws ServletException, IOException {
+        
+        final List<URI> destUris = new ArrayList<>();
+        
+        final URI requestUri = invite.getRequestURI();
+        
+        if (requestUri instanceof SipURI) {
+            final String canonicalizedUri = Util.canonicalizeUri((SipURI) requestUri.clone()).toString();
+            final List<Binding> bindings = getBindingsManager().getBindings(canonicalizedUri);
+            if (bindings != null) {
+                for (final Binding binding : bindings) {
+                    final URI destUri = getSipFactory().createURI(binding.getContactAddress());
+                    destUris.add(destUri);
+                }
+            }
+        }
+        
+        if (destUris.isEmpty()) {
+            destUris.add(requestUri);
+        }
+        
+        final Proxy proxy = invite.getProxy();
+        proxy.proxyTo(destUris);
+    }
+}

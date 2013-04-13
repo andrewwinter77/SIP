7797e082918d074cac2f1f2b1d412af4dac86380
andrew <andrew@192.168.1.65>
2013-04-13 11:22:58 +0100

    Added missing module dependency.

--- Tools/deployToJBossEap61.sh
+++ Tools/deployToJBossEap61.sh
@@ -24,7 +24,7 @@ ${JBOSS_HOME}/bin/jboss-cli.sh --connect command="xa-data-source --name=sipappDS
 
 ${JBOSS_HOME}/bin/jboss-cli.sh --connect command="module remove --name=org.andrewwinter.jsr289.jboss"
 ${JBOSS_HOME}/bin/jboss-cli.sh --connect command="module add --name=org.andrewwinter.jsr289.jboss --resources=/Users/andrew/.m2/repository/org/andrewwinter/sip/JBossEap61Module/1.0-SNAPSHOT/JBossEap61Module-1.0-SNAPSHOT-jar-with-dependencies.jar --dependencies=
-javax.api,org.jboss.staxmapper,org.jboss.as.ee,org.jboss.as.controller,org.jboss.as.naming,org.jboss.as.server,org.jboss.modules,org.jboss.msc,org.jboss.logging,org.jboss.vfs,javax.xml.bind.api,org.jboss.jandex,javax.servlet.sip.api,javax.annotation.api,org.jboss.netty,org.jboss.metadata,org.jboss.as.web,org.jboss.as.weld"
+javax.api,org.jboss.staxmapper,org.jboss.as.ee,org.jboss.as.controller,org.jboss.as.naming,org.jboss.as.server,org.jboss.modules,org.jboss.msc,org.jboss.logging,org.jboss.vfs,javax.xml.bind.api,org.jboss.jandex,javax.servlet.sip.api,javax.annotation.api,org.jboss.netty,org.jboss.metadata,org.jboss.as.web,org.jboss.as.weld,javax.enterprise.api"
 
 ${JBOSS_HOME}/bin/jboss-cli.sh --connect command="/extension=org.andrewwinter.jsr289.jboss:add"
 ${JBOSS_HOME}/bin/jboss-cli.sh --connect command="/subsystem=jsr289:add"

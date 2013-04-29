48e86e4b6e86f402e81875de2a2432adcfda68f8
andrew <andrew@192.168.1.65>
2013-04-29 23:19:13 +0100

    Code tidying.

--- JBossEap61Module/src/main/java/org/andrewwinter/jsr289/jboss/deployment/phase/ServletContextFactory.java
+++ JBossEap61Module/src/main/java/org/andrewwinter/jsr289/jboss/deployment/phase/ServletContextFactory.java
@@ -1,18 +1,8 @@
 package org.andrewwinter.jsr289.jboss.deployment.phase;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletContext;
-import javax.servlet.sip.SipFactory;
-import javax.servlet.sip.SipServlet;
-import javax.servlet.sip.SipURI;
-import org.andrewwinter.jsr289.SipServletRequestHandler;
 import org.andrewwinter.jsr289.api.ServletContextProvider;
-import org.andrewwinter.jsr289.api.SipFactoryImpl;
-import org.andrewwinter.jsr289.api.SipSessionsUtilImpl;
-import org.andrewwinter.jsr289.api.TimerServiceImpl;
 import org.andrewwinter.jsr289.jboss.deployment.attachment.CustomAttachments;
 import org.andrewwinter.jsr289.jboss.metadata.SipModuleInfo;
 import org.apache.catalina.core.StandardContext;
@@ -49,22 +39,5 @@ public class ServletContextFactory implements WebContextFactory {
         
         // UasActiveServlet in the 289 TCK checks this is present.
         context.setAttribute(ServletContext.TEMPDIR, du.getAttachment(CustomAttachments.TEMP_DIRECTORY));
-    }
-    
-    private static List<SipURI> createOutboundInterfaceList(final SipFactory sf) {
-        final List<SipURI> result = new ArrayList<>();
-        try {
-            InetAddress localhost = InetAddress.getLocalHost();
-            InetAddress[] allMyIps = InetAddress.getAllByName(localhost.getCanonicalHostName());
-            if (allMyIps != null) {
-                for (final InetAddress addr : allMyIps) {
-                    result.add(sf.createSipURI(null, addr.getHostAddress()));
-                }
-            }
-        } catch (UnknownHostException e) {
-            System.out.println("Exception enumerating IPs.");
-        }
-        
-        return result;
     }
 }
